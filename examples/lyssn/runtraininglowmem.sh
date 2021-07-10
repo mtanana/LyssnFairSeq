0e40440a1ba3252674682f299fa7e047aa000c69
mtanana <mike@lyssn.io>
2021-07-10 15:36:07 -0600

    ASR training instructions

--- examples/lyssn/runtraininglowmem.sh
+++ examples/lyssn/runtraininglowmem.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES=0
 
 #for some reason I think this only uses the valid subset
 
-python ../../train.py /lyssn/datasets/asr/asroct2020/ --save-dir /lyssn/temp/asr/wav2vec/asroct20202.1/ --fp16 \
+python ../../train.py /lyssn/datasets/asr/asroct2020/ --save-dir /lyssn/temp/asr/wav2vec/asroct20202.2/ --fp16 \
 --reset-dataloader \
 --post-process letter --valid-subset valid  --no-epoch-checkpoints --best-checkpoint-metric wer --num-workers 15 \
 --max-update 4050000 --sentence-avg --task audio_pretraining --arch wav2vec_ctc --w2v-path /lyssn/datasets/asr/test2/wav2vec_vox.pt \
@@ -12,7 +12,7 @@ python ../../train.py /lyssn/datasets/asr/asroct2020/ --save-dir /lyssn/temp/asr
 --feature-grad-mult 0.0 --freeze-finetune-updates 10000 --validate-after-updates 10000 --optimizer adam \
 --adam-betas '(0.9, 0.98)' --adam-eps 1e-08 --lr 1e-05 --lr-scheduler tri_stage --warmup-steps 8000 --hold-steps 100000 \
 --decay-steps 2000000 --final-lr-scale 0.05 --final-dropout 0.0 --dropout 0.0 --activation-dropout 0.1 --criterion ctc \
---attention-dropout 0.0  --max-tokens 200000 --max-tokens-valid 300000   --seed 2337 --log-format tqdm --log-interval 100 --ddp-backend no_c10d \
+--attention-dropout 0.0  --max-tokens 2000000 --max-tokens-valid 3000000   --seed 2337 --log-format tqdm --log-interval 100 --ddp-backend no_c10d \
 --tensorboard-logdir /lyssn/temp/asr/wav2vec/test2/tensorboardoct20202v1/test1/ \
 
 
