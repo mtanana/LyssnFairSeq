61a786afb966e06aa4575d0cce805800812da6ee
mtanana <4827024+mtanana@users.noreply.github.com>
2023-07-09 19:09:31 -0600

    added back in custom function for dictionary fairseq

--- examples/wav2vec/unsupervised/tasks/unpaired_audio_text.py
+++ examples/wav2vec/unsupervised/tasks/unpaired_audio_text.py
@@ -332,6 +332,11 @@ class UnpairedAudioText(FairseqTask):
         """Return the :class:`~fairseq.data.Dictionary` for the language
         model."""
         return self._target_dictionary
+    
+    def set_target_dictionary(self, file_path):
+        print("Loading dict" + str(file_path))
+        self._target_dictionary = Dictionary.load(file_path)
+
 
     def max_positions(self):
         """Maximum input length supported by the encoder."""
